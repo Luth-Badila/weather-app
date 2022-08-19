8296152003373f07f641337389cd81a06ea99680
Luth-Badila <FrancescoLutfi10@gmail.com>
2022-08-19 10:30:31 +0700

    add first section in weather app aplication

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  env: {
+    NEXT_PUBLIC_WEATHER_KEY: process.env.NEXT_PUBLIC_WEATHER_KEY,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
