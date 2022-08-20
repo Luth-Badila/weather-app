ced4006cdca51f48a39ebf03fe89b0f3cc65457d
Luth-Badila <FrancescoLutfi10@gmail.com>
2022-08-20 11:13:44 +0700

    add loading and search section

---
+++ components/Weather.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Weather = ({ data }) => {
+  console.log(data);
+  return (
+    <div>
+      Weather
+      <div></div>
+    </div>
+  );
+};
+
+export default Weather;
